a11ea71be3f7c5cadf6a2a10e7f625e898f9d2d9
yzouj0031-hub <yzouj0031-hub@users.noreply.github.com>
2026-07-17 10:43:13 +0000

    画面大升级:ACES色调映射+实时阴影+后期滤镜+洗手间内部建模

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='di13jie-v2';
+const CACHE='di13jie-v3';
 const CORE=['.','index.html','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
