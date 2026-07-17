d2121e4cff384a15bc6d231c01fb80ecfcf5097d
Allen <yzouj0031@gmail.com>
2026-07-17 23:35:33 +1000

    添加双人房间码联机模式

--- scripts/prepare-web.mjs
+++ scripts/prepare-web.mjs
@@ -7,6 +7,7 @@ const rootDir = path.resolve(scriptDir, '..');
 const webDir = path.join(rootDir, 'www');
 
 const threeCdnUrl = 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js';
+const peerCdnUrl = 'https://unpkg.com/peerjs@1.5.5/dist/peerjs.min.js';
 const tracks = [
   {
     url: "https://raw.githubusercontent.com/yzouj0031-hub/bgm/main/Death%20Note%20-%20Yoshihisa%20Hirano%20And%20Hideki%20Taniuchi%20-%20L%27s%20Theme%20B.mp3",
@@ -40,7 +41,7 @@ async function download(url, destination) {
 await rm(webDir, { recursive: true, force: true });
 await mkdir(webDir, { recursive: true });
 
-for (const file of ['index.html', 'sw.js', 'manifest.webmanifest']) {
+for (const file of ['index.html', 'multiplayer.js', 'sw.js', 'manifest.webmanifest']) {
   await copyFile(path.join(rootDir, file), path.join(webDir, file));
 }
 await cp(path.join(rootDir, 'icons'), path.join(webDir, 'icons'), { recursive: true });
@@ -51,13 +52,19 @@ await copyFile(
   path.join(rootDir, 'node_modules', 'three', 'build', 'three.min.js'),
   path.join(webDir, 'vendor', 'three.min.js')
 );
+await copyFile(
+  path.join(rootDir, 'node_modules', 'peerjs', 'dist', 'peerjs.min.js'),
+  path.join(webDir, 'vendor', 'peerjs.min.js')
+);
 
 await Promise.all(
   tracks.map(({ url, localPath }) => download(url, path.join(webDir, localPath)))
 );
 
 let html = await readFile(path.join(webDir, 'index.html'), 'utf8');
-html = html.replaceAll(threeCdnUrl, 'vendor/three.min.js');
+html = html
+  .replaceAll(threeCdnUrl, 'vendor/three.min.js')
+  .replaceAll(peerCdnUrl, 'vendor/peerjs.min.js');
 for (const { url, localPath } of tracks) {
   html = html.replaceAll(url, localPath);
 }
@@ -65,8 +72,9 @@ await writeFile(path.join(webDir, 'index.html'), html);
 
 let serviceWorker = await readFile(path.join(webDir, 'sw.js'), 'utf8');
 serviceWorker = serviceWorker
-  .replaceAll('di13jie-v5', 'di13jie-apk-v1')
-  .replaceAll(threeCdnUrl, 'vendor/three.min.js');
+  .replaceAll('di13jie-v6', 'di13jie-apk-v2')
+  .replaceAll(threeCdnUrl, 'vendor/three.min.js')
+  .replaceAll(peerCdnUrl, 'vendor/peerjs.min.js');
 for (const { url, localPath } of tracks) {
   serviceWorker = serviceWorker.replaceAll(url, localPath);
 }
